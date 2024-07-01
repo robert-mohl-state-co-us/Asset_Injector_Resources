ecc96662430e7a63b624ffae879112537b5c673e
robert-mohl-state-co-us <robert.mohl@state.co.us>
2024-07-01 14:04:20 -0600

    Initial Commit of Asset Injector Files

---
+++ aria_hidden_fix.js
@@ -0,0 +1,27 @@
+hiddenPaginationValues = document.querySelectorAll('.pager-nav a span');
+
+		if(typeof hiddenPaginationValues !== 'undefined' && hiddenPaginationValues !== null && hiddenPaginationValues.length > 0 ) { 
+
+			console.log("Improperly hidden text found: "+hiddenPaginationValues.length);
+
+			var badPagearray = [...hiddenPaginationValues];
+
+			badPagearray.forEach(badPageElement => {
+
+				badPageElement.removeAttribute('aria-hidden');
+
+				if(badPageElement.innerHTML == "››") {
+
+					badPageElement.setAttribute('aria-label', 'Next');
+
+				}
+
+				if(badPageElement.innerHTML == "‹‹") {
+
+					badPageElement.setAttribute('aria-label', 'Previous');
+
+				}
+
+			});
+
+} 
